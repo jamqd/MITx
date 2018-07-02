ee9230f97d72524b81048d8e3785f7a275cbf22f
John Dang <john.amq.dang@gmail.com>
2018-07-01 23:22:18 -0700

    final done

---
+++ PSMidFin/asset-v1_MITx+6.00.2x_6+3T2016+type@asset+block@rabbits.py
@@ -0,0 +1,84 @@
+import random
+import pylab
+
+# Global Variables
+MAXRABBITPOP = 1000
+CURRENTRABBITPOP = 50
+CURRENTFOXPOP = 300
+
+def rabbitGrowth():
+    """ 
+    rabbitGrowth is called once at the beginning of each time step.
+
+    It makes use of the global variables: CURRENTRABBITPOP and MAXRABBITPOP.
+
+    The global variable CURRENTRABBITPOP is modified by this procedure.
+
+    For each rabbit, based on the probabilities in the problem set write-up, 
+      a new rabbit may be born.
+    Nothing is returned.
+    """
+    # you need this line for modifying global variables
+    global CURRENTRABBITPOP
+
+    prob = 1.0 - (CURRENTRABBITPOP/MAXRABBITPOP)
+    for n in range(CURRENTRABBITPOP):
+        if random.random() <= prob:
+            CURRENTRABBITPOP += 1
+            
+def foxGrowth():
+    """ 
+    foxGrowth is called once at the end of each time step.
+
+    It makes use of the global variables: CURRENTFOXPOP and CURRENTRABBITPOP,
+        and both may be modified by this procedure.
+
+    Each fox, based on the probabilities in the problem statement, may eat 
+      one rabbit (but only if there are more than 10 rabbits).
+
+    If it eats a rabbit, then with a 1/3 prob it gives birth to a new fox.
+
+    If it does not eat a rabbit, then with a 1/10 prob it dies.
+
+    Nothing is returned.
+    """
+    # you need these lines for modifying global variables
+    global CURRENTRABBITPOP
+    global CURRENTFOXPOP
+
+    prob = CURRENTRABBITPOP/MAXRABBITPOP
+    for n in range(CURRENTFOXPOP):
+        if random.random() <= prob and CURRENTRABBITPOP > 10:
+            CURRENTRABBITPOP -= 1
+            if random.random() <= 1/3:
+                CURRENTFOXPOP += 1
+        elif random.random() <= 0.9 and CURRENTFOXPOP > 10:
+            CURRENTFOXPOP -= 1
+            
+def runSimulation(numSteps):
+    """
+    Runs the simulation for `numSteps` time steps.
+
+    Returns a tuple of two lists: (rabbit_populations, fox_populations)
+      where rabbit_populations is a record of the rabbit population at the 
+      END of each time step, and fox_populations is a record of the fox population
+      at the END of each time step.
+
+    Both lists should be `numSteps` items long.
+    """
+    rabbitPop = []
+    foxPop= []
+    for n in range(numSteps):
+        rabbitGrowth()
+        foxGrowth()
+        rabbitPop.append(CURRENTRABBITPOP)
+        foxPop.append(CURRENTFOXPOP)
+    return (rabbitPop, foxPop)
+
+rab, fox = runSimulation(200)
+coeff = pylab.polyfit(range(len(rab)), rab, 2)
+pylab.plot(pylab.polyval(coeff, range(len(rab))), label = 'rabbits')
+coeff = pylab.polyfit(range(len(fox)), fox, 2)
+pylab.plot(pylab.polyval(coeff, range(len(fox))), label = 'foxes')
+pylab.legend(loc = 'best')
+pylab.show()
--- PSMidFin/bye.py
+++ PSMidFin/bye.py
@@ -69,13 +69,180 @@ def solveit(test):
             count += 1
 
 #### This test case prints 49 ####
-def f(x):
-    return (x+15)**0.5 + x**0.5 == 15
-print(solveit(f))
+# def f(x):
+#     return (x+15)**0.5 + x**0.5 == 15
+#print(solveit(f))
 
 #### This test case prints 0 ####
-def f(x):
-    return x == 0
-print(solveit(f))
+# def f(x):
+#     return x == 0
+# print(solveit(f))
 
 #Final
+
+#import random
+def drawing_without_replacement_sim(numTrials):
+    '''
+    Runs numTrials trials of a Monte Carlo simulation
+    of drawing 3 balls out of a bucket containing
+    4 red and 4 green balls. Balls are not replaced once
+    drawn. Returns a float - the fraction of times 3
+    balls of the same color were drawn in the first 3 draws.
+    '''
+    suc = 0
+    for n in range(numTrials):
+        bucket = [1,1,1,1,0,0,0,0]
+        drawn = []
+        for i in range(3):
+            a = random.choice(bucket)
+            drawn.append(a)
+            bucket.remove(a)
+        if sum(drawn) == 0 or sum(drawn) == 3:
+            suc += 1
+    return suc/numTrials
+
+#print(drawing_without_replacement_sim(100))
+
+import random, pylab
+
+
+# You are given this function
+def getMeanAndStd(X):
+    mean = sum(X) / float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean) ** 2
+    std = (tot / len(X)) ** 0.5
+    return mean, std
+
+
+# You are given this class
+class Die(object):
+    def __init__(self, valList):
+        """ valList is not empty """
+        self.possibleVals = valList[:]
+
+    def roll(self):
+        return random.choice(self.possibleVals)
+
+
+# Implement this -- Coding Part 1 of 2
+def makeHistogram(values, numBins, xLabel, yLabel, title=None):
+    """
+      - values, a sequence of numbers
+      - numBins, a positive int
+      - xLabel, yLabel, title, are strings
+      - Produces a histogram of values with numBins bins and the indicated labels
+        for the x and y axis
+      - If title is provided by caller, puts that title on the figure and otherwise
+        does not title the figure
+    """
+    if title != None:
+        pylab.title(title)
+    pylab.hist(values, bins=numBins)
+    pylab.xlabel(xLabel)
+    pylab.ylabel(yLabel)
+    pylab.show()
+
+#makeHistogram([1,2,3,3,4,5,6,6,6,7,8,8,8,8,8,8,9], 4, 'x', 'y')
+
+# Implement this -- Coding Part 2 of 2
+def getAverage(die, numRolls, numTrials):
+    """
+      - die, a Die
+      - numRolls, numTrials, are positive ints
+      - Calculates the expected mean value of the longest run of a number
+        over numTrials runs of numRolls rolls
+      - Calls makeHistogram to produce a histogram of the longest runs for all
+        the trials. There should be 10 bins in the histogram
+      - Choose appropriate labels for the x and y axes.
+      - Returns the mean calculated
+    """
+    longestRuns = []
+    for n in range(numTrials):
+        rolled = []
+        for i in range(numRolls):
+            rolled.append(die.roll())
+        longestCount = 0
+        currentCount = 0
+        current = -1
+        for j in range(len(rolled)):
+            if j == 0:
+                currentCount = 1
+                current = rolled[j]
+                longestCount = 1
+            else:
+                if rolled[j] == current:
+                    currentCount += 1
+                    if currentCount > longestCount:
+                        longestCount = currentCount
+                else:
+                    if currentCount > longestCount:
+                        longestCount = currentCount
+                    current = rolled[j]
+                    currentCount = 1
+        longestRuns.append(longestCount)
+    makeHistogram(longestRuns, 10, 'Longest Runs', 'Frequency')
+    return sum(longestRuns)/len(longestRuns)
+
+
+# One test case
+#print(getAverage(Die([1, 2, 3, 4, 5, 6, 6, 6, 7]), 500, 10000))
+
+import numpy as np
+def find_combination(choices, total):
+    """
+    choices: a non-empty list of ints
+    total: a positive int
+
+    Returns result, a numpy.array of length len(choices)
+    such that
+        * each element of result is 0 or 1
+        * sum(result*choices) == total
+        * sum(result) is as small as possible
+    In case of ties, returns any result that works.
+    If there is no result that gives the exact total,
+    pick the one that gives sum(result*choices) closest
+    to total without going over.
+    """
+    # bestSum = -1
+    # best = np.array([])
+    # for n in range(len(choices) + 1):
+    #     template = []
+    #     for i in range(n):
+    #         template.append(1)
+    #     for v in range(len(choices) - n):
+    #         template.append(0)
+    #     perms = list(itertools.permutations(template, len(choices)))
+    #     for d in range(len(perms)):
+    #         perms[d] = np.array(perms[d])
+    #     for b in perms:
+    #         sum = np.sum(np.array(b) * np.array(choices))
+    #         if sum == total:
+    #             return b
+    #         elif sum < total and sum > bestSum:
+    #             best = np.array(b)
+    #             bestSum = sum
+    # return best
+    bestSum = -1
+    best = []
+    for n in range(len(choices) + 1):
+        for i in range(len(choices) - n + 1):
+            result = []
+            for k in range(i):
+                result.append(0)
+            for j in range(n):
+                result.append(1)
+            for h in range(len(choices) - n - i):
+                result.append(0)
+            sum = np.sum(np.array(choices) * np.array(result))
+            if sum == total:
+                return np.array(result)
+            elif sum < total and sum > bestSum:
+                best = np.array(result)
+                bestSum = sum
+    return best
+
+a = find_combination([45,6,7,4,9], 2)
+print(a)
+print(type(a))
